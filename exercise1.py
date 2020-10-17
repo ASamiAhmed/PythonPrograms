7f5a88c4f7579e7bc5ce6af6c7fc9764b01947ed
ASamiAhmed <72922597+ASamiAhmed@users.noreply.github.com>
2020-10-17 16:19:36 +0500

    practice if_else

---
+++ exercise1.py
@@ -0,0 +1,7 @@
+yellow = input("is first ball is yellow? (y/n)")
+blue = input("is second ball is blue? (y/n)")
+if (yellow == "y") and (blue == "y"):
+    print("both balls are present here ")
+else: 
+    print("no")    
+
