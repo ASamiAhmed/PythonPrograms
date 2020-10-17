33770346c5120c82a16083e9a7817b85369b33f3
ASamiAhmed <72922597+ASamiAhmed@users.noreply.github.com>
2020-10-17 15:53:59 +0500

    working with if

---
+++ check.py
@@ -0,0 +1,23 @@
+weight1 = 100
+weight2 = 200
+'''
+print(weight1 == weight2)
+print(weight1 != weight2)
+print(weight1 < weight2)
+print(weight1 > weight2)
+print(weight1 <= weight2)
+print(weight1 >= weight2) 
+'''
+
+if weight1 == weight2:
+  print("if is working")
+if weight1 != weight2:
+  print(" 2nd if is working") 
+if weight1 < weight2:
+  print(" 3rd if is working")  
+if weight1 > weight2:
+  print( "4th if is working")
+if weight1 <= weight2:
+  print ( "5th if is working") 
+if weight1 >= weight2:
+  print("6th if is working")     
