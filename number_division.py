d933363e401fce303ca7ae0c0c6584bfaa0f015d
ASamiAhmed <72922597+ASamiAhmed@users.noreply.github.com>
2020-10-19 15:39:52 +0500

    number_divisibility

---
+++ number_division.py
@@ -0,0 +1,6 @@
+number1 = int(input("enter first no:" ))
+number2 = int(input("enter second no:" ))
+if number1 % number2 == 0:
+    print("number is divisible")
+else:
+    print("number is not divisible")
