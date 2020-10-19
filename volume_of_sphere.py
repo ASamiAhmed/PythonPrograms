aa9c681c161b8afc22b17d4278fa8094da554872
ASamiAhmed <72922597+ASamiAhmed@users.noreply.github.com>
2020-10-19 16:10:29 +0500

    volume_

---
+++ volume_of_sphere.py
@@ -0,0 +1,8 @@
+'''
+Write a Python program to get the volume of a sphere, please take the
+radius as input from user. V=4 / 3 πr3
+'''
+pie = 3.14
+radius = int(input("Enter radius: "))
+volume = ( 4/3)*pie*radius**3
+print("The volume of sphere is",volume ) 
