b6df9b9ca3ff14490f15fba16b4bc52a22f8c203
Sami_Ahmed <72922597+ASamiAhmed@users.noreply.github.com>
2020-10-20 15:59:15 +0500

    Add files via upload

---
+++ string_concatenation.py
@@ -0,0 +1,9 @@
+name = "sami"
+age = 18
+grade = 8
+height = 5.8
+is_student = True
+print("My name is",name,"my age is",age,"I am student",is_student,"I am in grade",grade,"my height is",height)
+print("My name is"+name+" my age is "+str(age)+" I am student "+str(is_student)+" I am in grade "+str(grade)+" my height is "+str(height))
+print("My name is {} my age is {} age I am student {} I am in grade {} my height is {}".format(name,age,is_student,grade,height))
+print(f"My name is {name} my age is {age} I am student {is_student} I am in grade {grade} my height is {height}")
