1a2f1adb9a73f007b59552e5967757107b41ad1d
Sami_Ahmed <72922597+ASamiAhmed@users.noreply.github.com>
2020-10-22 18:50:16 +0500

    Add files via upload

---
+++ for_loop_with_strings.py
@@ -0,0 +1,27 @@
+#  name = "apple"
+# for i in name:
+#     print(i)
+
+# name = "apple"
+# for i in range(0,len(name)):
+#    print(name[i])
+
+# name = "hello world"
+# print(name[4])
+#  print(name[5])
+#  print(name[-1])
+
+# name = "hello world"
+#  print(name[0:5])
+
+#  name = "sami"
+#  for i in name:
+#   print(i,end="")
+
+#  name = "sami"
+#  print(name[0:4])
+
+count = 0
+for loop in range(0,11):
+    print(count)
+    count= count + 1
