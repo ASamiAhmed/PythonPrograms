9ea56cc40350d7848c7518d29905ec893bd772e0
Sami_Ahmed <72922597+ASamiAhmed@users.noreply.github.com>
2020-10-23 13:19:37 +0500

    even odd identification

---
+++ even_odd.py
@@ -0,0 +1,7 @@
+#Write a Python program to find whether a given number (accept from the
+# user) is even or odd, print out an appropriate message to the user
+number = int(input("enter number: "))
+if number%2==0:
+   print("The number you entered is even")
+else:
+    print("The number you entered is odd")    
