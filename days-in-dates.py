e2db740c3f2f977b6d77e19cb83f02479bba828a
Sami_Ahmed <72922597+ASamiAhmed@users.noreply.github.com>
2020-10-23 12:55:59 +0500

    Add files via upload

---
+++ days-in-dates.py
@@ -0,0 +1,5 @@
+#Write a Python program to calculate number of days between two dates
+first_date =int(input("enter first date: "))
+second_date = int(input("enter second date: "))
+days = first_date - second_date
+print("number of days in between two dates is ",days)
