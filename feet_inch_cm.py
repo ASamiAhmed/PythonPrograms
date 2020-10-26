bf3d404c0488df4ef0a704a19dbfa5808f706af3
Sami_Ahmed <72922597+ASamiAhmed@users.noreply.github.com>
2020-10-26 12:41:07 +0500

    feet and inch into cm

---
+++ feet_inch_cm.py
@@ -0,0 +1,9 @@
+'''
+Write a Python program to convert height (in feet and inches) to
+centimetres.
+'''
+number = int(input("Enter number: "))
+feet = number*30.48
+print("The number you entered in centimeter is",feet)
+inch = number*2.54
+print("The number you entered in centimeter is",inch)
